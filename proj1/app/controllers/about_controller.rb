8778fe4f5b696430f2105375b3a8ba2f008dc888
Ravneet Singh <rssinghji@gmail.com>
2017-12-24 23:30:17 -0600

    Adding Neew Project

---
+++ proj1/app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+  	@about_text = 'Welcome to my About Page!'
+  end
+end
---
+++ proj1/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'posts' => 'posts#index'
+
+  get 'about' => 'about#index'
+
+  root 'about#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
